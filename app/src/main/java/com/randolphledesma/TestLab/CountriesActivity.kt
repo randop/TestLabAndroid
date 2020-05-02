52c32601b0944c218ed771e750e670092c11c1a9
Randolph <randop@me.com>
2020-05-02 19:56:15 +0800

    updates

--- app/src/main/java/com/randolphledesma/TestLab/CountriesActivity.kt
+++ app/src/main/java/com/randolphledesma/TestLab/CountriesActivity.kt
@@ -1,5 +1,6 @@
 package com.randolphledesma.TestLab
 
+import android.app.Activity
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
@@ -8,12 +9,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_countries.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.util.*
 
 class CountriesActivity : AppCompatActivity() {
@@ -42,23 +48,32 @@ class CountriesActivity : AppCompatActivity() {
             setIconifiedByDefault(false)
         }
         val searchText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
-
-        searchText.setOnEditorActionListener{ v, actionId, event ->
-            println("onSearch")
-            searchView.onq
+        searchText.setOnEditorActionListener{ view, actionId, event ->
+            GlobalScope.launch(Dispatchers.Main) {
+                doCountrySearch(view.text.toString())
+            }
+            /** hide keyboard **/
+            val imm = applicationContext.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
             true
         }
         return super.onCreateOptionsMenu(menu)
     }
 
     private fun handleIntent(intent: Intent) {
-        val countryList = country_list as RecyclerView
-        countryList.layoutManager = LinearLayoutManager(this)
         var query = ""
         if (Intent.ACTION_SEARCH == intent.action) {
             query = intent.getStringExtra(SearchManager.QUERY)
         }
 
+        GlobalScope.launch(Dispatchers.Main) {
+            doCountrySearch(query)
+        }
+    }
+    
+    private suspend fun doCountrySearch(query: String) {
+        val countryList = country_list as RecyclerView
+        countryList.layoutManager = LinearLayoutManager(this)
         val countries = allCountries.filter {
             if (query.isNotBlank()) {
                 it.toLowerCase().contains(query.toLowerCase())
